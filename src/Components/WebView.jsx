bea3f38736e7bf88de51656c6134b9b5397b76d6
IMRANDIL <aliimranadil2@gmail.com>
2023-01-29 14:37:29 +0530

    some more

--- src/Components/WebView.jsx
+++ src/Components/WebView.jsx
@@ -1,27 +1,29 @@
 import {View, Text, ActivityIndicator, Dimensions} from 'react-native';
-import React, {useEffect, useLayoutEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {WebView} from 'react-native-webview';
-
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
 
 export default function WebViewComponent(props) {
   const [loading, setLoading] = useState(true);
-  useLayoutEffect(() => {
-    setLoading(false);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000); // 5 seconds
   }, []);
-  return loading ? (
-    <ActivityIndicator
-      size={80}
-      color="black"
-      style={{
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: deviceWidth,
-        height: deviceHeight,
-      }}
-    />
-  ) : (
-    <WebView source={{uri: `${props.route.params.url}`}} />
-  );
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: deviceWidth,
+          height: deviceHeight,
+        }}>
+        <ActivityIndicator size={80} color="black" />
+      </View>
+    );
+  }
+
+  return <WebView source={{uri: props.route.params.url}} />;
 }
