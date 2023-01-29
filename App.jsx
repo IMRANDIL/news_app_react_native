4cdeec289d55ecc20e9cd557ac37ab81750df5be
IMRANDIL <aliimranadil2@gmail.com>
2023-01-29 11:26:05 +0530

    some more change

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/Screens/HomeScreen';
+import GetNews from './src/Screens/GetNews';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Trending" component={HomeScreen} />
+        <Stack.Screen name="GetNews" component={GetNews} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/Screens/GetNews.jsx
+++ src/Screens/GetNews.jsx
@@ -1,10 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-export default function GetNews() {
+export default function GetNews(props) {
   return (
     <View>
-      <Text>GetNews</Text>
+      <Text>{props.route.params.category}</Text>
     </View>
   );
 }
