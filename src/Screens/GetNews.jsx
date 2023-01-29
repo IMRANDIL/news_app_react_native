f2ef4ef2518d7ea4b0729c82fac4584ef9fc5635
IMRANDIL <aliimranadil2@gmail.com>
2023-01-29 11:43:49 +0530

    some more change

--- src/Screens/GetNews.jsx
+++ src/Screens/GetNews.jsx
@@ -1,11 +1,26 @@
 import {View, Text} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import config from '../../config/config';
 
 export default function GetNews(props) {
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState(false);
   useEffect(() => {
     props.navigation.setOptions({
       title: props.route.params.category,
     });
+
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=${config.API_KEY}`,
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        setNews(data.articles);
+      })
+      .catch(error => {
+        setError(true);
+        console.log(error);
+      });
   }, []);
 
   return (
